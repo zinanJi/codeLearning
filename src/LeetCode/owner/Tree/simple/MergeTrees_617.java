a6fc94d510d1c5c760910dbc67528749bf06ad05
zinan.ji <827025200@qq.com>
2020-05-05 02:16:12 +0800

    [github#3] 3道simple树题

---
+++ src/LeetCode/owner/Tree/simple/MergeTrees_617.java
@@ -0,0 +1,13 @@
+package LeetCode.owner.Tree.simple;
+
+import LeetCode.owner.Tree.TreeNode;
+
+/**
+ * Created by zinan.ji on 2020-05-05.
+ * 617. 合并二叉树
+ */
+public class MergeTrees_617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return null;
+    }
+}
